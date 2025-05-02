910efb474011e007344a6137daf7c2ee5fdd037c
Paul Laffitte <paul.laffitte@enix.fr>
2025-05-02 15:09:25 +0200

    feat: update node annotations

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/enix/topomatik/internal/autodiscovery/lldp"
 	"github.com/enix/topomatik/internal/config"
 	"github.com/enix/topomatik/internal/controller"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	var configPath string
+	var (
+		configPath     string
+		kubeconfigPath string
+	)
 
 	flag.StringVar(&configPath, "config", "/etc/topomatik/config.yaml", "Path to the configuration file")
+	flag.StringVar(&kubeconfigPath, "kubeconfig", "", "Path to a kubeconfig file.")
 	flag.Parse()
 
 	config, err := config.Load(configPath)
@@ -19,7 +27,25 @@ func main() {
 		panic(err)
 	}
 
-	ctrl, err := controller.New(config.AnnotationTemplates)
+	var kubeconfig *rest.Config
+
+	if kubeconfigPath == "" {
+		fmt.Println("using in-cluster configuration")
+		kubeconfig, err = rest.InClusterConfig()
+	} else {
+		fmt.Println("using configuration from file: " + kubeconfigPath)
+		kubeconfig, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	k8sClientset, err := kubernetes.NewForConfig(kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	ctrl, err := controller.New(k8sClientset, config.AnnotationTemplates)
 	if err != nil {
 		panic(err)
 	}
--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -2,19 +2,25 @@ package controller
 
 import (
 	"bytes"
+	"context"
 	"fmt"
+	"os"
 	"text/template"
 
 	"github.com/enix/topomatik/internal/autodiscovery"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 type Controller struct {
+	clientset           *kubernetes.Clientset
 	annotationTemplates map[string]*template.Template
 	services            map[string]*autodiscovery.EngineHandler
 }
 
-func New(annotationTemplates map[string]string) (_ *Controller, err error) {
+func New(clientset *kubernetes.Clientset, annotationTemplates map[string]string) (_ *Controller, err error) {
 	controller := &Controller{
+		clientset:           clientset,
 		annotationTemplates: map[string]*template.Template{},
 		services:            map[string]*autodiscovery.EngineHandler{},
 	}
@@ -35,6 +41,8 @@ func (c *Controller) Register(name string, service autodiscovery.Engine) {
 
 func (c *Controller) Start() error {
 	update := make(chan struct{})
+	nodeName := os.Getenv("NODE_NAME")
+	println("NODE_NAME:", nodeName)
 
 	for _, service := range c.services {
 		if err := service.Start(); err != nil {
@@ -50,14 +58,27 @@ func (c *Controller) Start() error {
 			data[name] = service.Data
 		}
 
+		node, err := c.clientset.CoreV1().Nodes().Get(context.Background(), nodeName, metav1.GetOptions{})
+		if err != nil {
+			fmt.Printf("could not get node %s: %s", nodeName, err.Error())
+			continue
+		}
+
 		for annotation, tmpl := range c.annotationTemplates {
 			value := &bytes.Buffer{}
 			err := tmpl.Execute(value, data)
 			if err != nil {
-				fmt.Printf("could not render template for %s: %s", annotation, err.Error())
+				fmt.Printf("could not render template for %s: %s\n", annotation, err.Error())
 			} else {
+				node.Annotations[annotation] = value.String()
 				fmt.Printf("%s: %s\n", annotation, value)
 			}
+		}
+
+		_, err = c.clientset.CoreV1().Nodes().Update(context.Background(), node, metav1.UpdateOptions{})
+		if err != nil {
+			fmt.Printf("could not update node %s: %s\n", nodeName, err.Error())
+			continue
 		}
 
 		fmt.Println("")
