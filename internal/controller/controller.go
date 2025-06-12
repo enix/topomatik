be396028460ac6dbb659598ff911c54e81745fdf
Paul Laffitte <paul.laffitte@enix.fr>
2025-06-12 18:18:59 +0200

    feat: watch node and reconcile on change

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -10,15 +10,19 @@ import (
 
 	"github.com/Masterminds/sprig"
 	"github.com/enix/topomatik/internal/autodiscovery"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 )
 
 type Controller struct {
+	nodeName       string
 	clientset      *kubernetes.Clientset
 	labelTemplates map[string]*template.Template
 	engines        map[string]autodiscovery.Engine
+	discoveryData  map[string]map[string]string
 }
 
 type EnginePayload struct {
@@ -28,9 +32,11 @@ type EnginePayload struct {
 
 func New(clientset *kubernetes.Clientset, labelTemplates map[string]string) (*Controller, error) {
 	controller := &Controller{
+		nodeName:       os.Getenv("NODE_NAME"),
 		clientset:      clientset,
 		labelTemplates: map[string]*template.Template{},
 		engines:        map[string]autodiscovery.Engine{},
+		discoveryData:  make(map[string]map[string]string),
 	}
 
 	for label, tmpl := range labelTemplates {
@@ -48,8 +54,7 @@ func (c *Controller) Register(name string, engine autodiscovery.Engine) {
 }
 
 func (c *Controller) Start() error {
-	nodeName := os.Getenv("NODE_NAME")
-	println("NODE_NAME:", nodeName)
+	println("NODE_NAME:", c.nodeName)
 
 	dataChannel := make(chan EnginePayload)
 
@@ -68,50 +73,84 @@ func (c *Controller) Start() error {
 		go engine.Watch(callback)
 	}
 
-	for payload := range dataChannel {
-		data := map[string]map[string]string{}
-		data[payload.EngineName] = payload.Data
+	c.watchNode()
 
-		node, err := c.clientset.CoreV1().Nodes().Get(context.Background(), nodeName, metav1.GetOptions{})
+	for payload := range dataChannel {
+		c.discoveryData[payload.EngineName] = payload.Data
+
+		node, err := c.clientset.CoreV1().Nodes().Get(context.Background(), c.nodeName, metav1.GetOptions{})
 		if err != nil {
-			fmt.Printf("could not get node %s: %s", nodeName, err.Error())
+			fmt.Printf("could not get node %s: %s", c.nodeName, err.Error())
 			continue
 		}
 
-		labels := map[string]string{}
-		for label, tmpl := range c.labelTemplates {
-			value := &bytes.Buffer{}
-			err := tmpl.Execute(value, data)
-			if err != nil {
-				fmt.Printf("could not render template for %s: %s\n", label, err.Error())
-			} else {
-				if node.Labels[label] != value.String() {
-					labels[label] = value.String()
-					fmt.Printf("%s: %s\n", label, value)
-				}
+		if err := c.reconcileNode(node); err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+
+	return nil
+}
+
+func (c *Controller) watchNode() error {
+	watchInterface, err := c.clientset.CoreV1().Nodes().Watch(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for event := range watchInterface.ResultChan() {
+			if event.Type != watch.Modified {
+				continue
+			}
+
+			node, ok := event.Object.(*corev1.Node)
+			if !ok || node.Name != c.nodeName {
+				continue
+			}
+
+			fmt.Println("received a node update, triggering reconciliation")
+			if err := c.reconcileNode(node); err != nil {
+				fmt.Println(err.Error())
 			}
 		}
+	}()
 
-		if len(labels) == 0 {
-			continue
-		}
+	return nil
+}
 
-		patch := map[string]interface{}{
-			"metadata": map[string]interface{}{
-				"labels": labels,
-			},
-		}
-		patchBytes, err := json.Marshal(patch)
+func (c *Controller) reconcileNode(node *corev1.Node) error {
+	labels := map[string]string{}
+	for label, tmpl := range c.labelTemplates {
+		value := &bytes.Buffer{}
+		err := tmpl.Execute(value, c.discoveryData)
 		if err != nil {
-			fmt.Printf("failed to marshal patch: %s\n", err)
-			continue
+			fmt.Printf("could not render template for %s: %s\n", label, err.Error())
+		} else {
+			if node.Labels[label] != value.String() {
+				labels[label] = value.String()
+				fmt.Printf("%s: %s\n", label, value)
+			}
 		}
+	}
 
-		_, err = c.clientset.CoreV1().Nodes().Patch(context.Background(), node.Name, types.MergePatchType, patchBytes, metav1.PatchOptions{})
-		if err != nil {
-			fmt.Printf("could not update node %s: %s\n", nodeName, err.Error())
-			continue
-		}
+	if len(labels) == 0 {
+		return nil
+	}
+
+	patch := map[string]interface{}{
+		"metadata": map[string]interface{}{
+			"labels": labels,
+		},
+	}
+	patchBytes, err := json.Marshal(patch)
+	if err != nil {
+		return fmt.Errorf("failed to marshal patch: %w", err)
+	}
+
+	_, err = c.clientset.CoreV1().Nodes().Patch(context.Background(), node.Name, types.MergePatchType, patchBytes, metav1.PatchOptions{})
+	if err != nil {
+		return fmt.Errorf("could not patch node %s: %w", c.nodeName, err)
 	}
 
 	return nil
